22ede70a79e722dc66f0744f9421150cbe5a64dd
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-05-26 16:49:46 +0300

    get all user subscriptions

--- src/actions/subscriptions.ts
+++ src/actions/subscriptions.ts
@@ -67,3 +67,32 @@ export const getCurSubscription = async (user_id: string) => {
     };
   }
 };
+
+export const getAllUserSubscription = async (user_id: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("subscriptions")
+      .select("* , plans(*)")
+      .eq("user_id", user_id)
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    let formattedData = data.map((item: any) => ({
+      plan: item.plans,
+      ...item,
+    }));
+
+    return {
+      success: true,
+      data: formattedData,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
--- src/app/(private)/account/user/subscriptions/page.tsx
+++ src/app/(private)/account/user/subscriptions/page.tsx
@@ -1,9 +1,99 @@
+"use client";
+import { getAllUserSubscription } from "@/actions/subscriptions";
 import PageTitle from "@/components/ui/page-title";
+import Spinner from "@/components/ui/spinner";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import usersStore, { IUsersStore } from "@/store/users-store";
+import { ISubscription } from "@/types/subscription";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const Page = () => {
+  const [subscriptions, setSubscription] = useState<ISubscription[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { user } = usersStore() as IUsersStore;
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const res: any = await getAllUserSubscription(user?.id!);
+      if (!res.success) {
+        throw new Error(res.message);
+      }
+      setSubscription(res?.data);
+    } catch (e: any) {
+      toast.error(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const columns = [
+    "Subscription ID",
+    "Purchase Date",
+    "Start Date",
+    "End Date",
+    "Plan",
+    "Amount",
+    "Payment Id",
+  ];
+
   return (
     <div>
       <PageTitle title="My Subscriptions" />
+
+      {loading && <Spinner height={150} />}
+
+      {!subscriptions.length && !loading && (
+        <p className="text-sm text-gray-600">
+          You do not have any subscription at the moment.
+        </p>
+      )}
+
+      {subscriptions.length > 0 && !loading && (
+        <Table>
+          <TableHeader>
+            <TableRow className="bg-gray-100">
+              {columns.map((column) => (
+                <TableHead className="font-bold" key={column}>
+                  {column}
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {subscriptions.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.id}</TableCell>
+                <TableCell>
+                  {dayjs(item.created_at).format("MMM DD, YYYY")}
+                </TableCell>
+                <TableCell>
+                  {dayjs(item.start_date).format("MMM DD, YYYY")}
+                </TableCell>
+                <TableCell>
+                  {dayjs(item.end_date).format("MMM DD, YYYY")}
+                </TableCell>
+                <TableCell>{item.plan?.name}</TableCell>
+                <TableCell>$ {item.amount}</TableCell>
+                <TableCell>{item.payment_id}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </div>
   );
 };
